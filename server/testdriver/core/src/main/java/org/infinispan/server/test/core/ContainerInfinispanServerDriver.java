8a26dd7bbc53f9c743802429663f653e189e66d9
Diego Lovison <diegolovison@gmail.com>
2020-06-12 11:39:33 -0300

    Docker memory config

--- server/testdriver/core/src/main/java/org/infinispan/server/test/core/ContainerInfinispanServerDriver.java
+++ server/testdriver/core/src/main/java/org/infinispan/server/test/core/ContainerInfinispanServerDriver.java
@@ -274,9 +274,12 @@ public class ContainerInfinispanServerDriver extends AbstractInfinispanServerDri
       }
 
       GenericContainer container = new GenericContainer<>(image)
-         .withCreateContainerCmdModifier(cmd -> cmd.getHostConfig().withMounts(
-            Arrays.asList(new Mount().withSource(this.volumes[i]).withTarget(serverPath()).withType(MountType.VOLUME))
-         ));
+         .withCreateContainerCmdModifier(cmd -> {
+            cmd.getHostConfig().withMounts(
+                  Arrays.asList(new Mount().withSource(this.volumes[i]).withTarget(serverPath()).withType(MountType.VOLUME))
+            );
+            cmd.getHostConfig().withMemory((long) 2 * 1024 * 1024).withMemorySwap((long) 1 * 1024 * 1024);
+         });
       // Process any enhancers
       container.withLogConsumer(new JBossLoggingConsumer(LogFactory.getLogger(name)).withPrefix(Integer.toString(i)));
       for (Consumer<OutputFrame> consumer : logConsumers)
