aea11e483c89d71fd519864686c0f2c5ed2c9fe2
Diego Lovison <diegolovison@gmail.com>
2021-07-12 10:53:12 -0300

    Example

--- integrationtests/server-integration/server-integration-commons/src/test/java/org/infinispan/test/integration/embedded/AbstractInfinispanCoreIT.java
+++ integrationtests/server-integration/server-integration-commons/src/test/java/org/infinispan/test/integration/embedded/AbstractInfinispanCoreIT.java
@@ -1,6 +1,8 @@
 package org.infinispan.test.integration.embedded;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
@@ -28,10 +30,18 @@ public abstract class AbstractInfinispanCoreIT {
 
    @Test
    public void testCacheManager() {
+      assertTrue("security.manager", Boolean.valueOf(System.getProperty("security.manager")));
+      assertNotNull("manager", System.getSecurityManager());
+
       GlobalConfigurationBuilder gcb = new GlobalConfigurationBuilder();
       gcb.defaultCacheName("default");
 
-      cm = new DefaultCacheManager(gcb.build(), new ConfigurationBuilder().build());
+      ConfigurationBuilder b = new ConfigurationBuilder();
+      b.persistence()
+            .addSingleFileStore()
+            .maxEntries(5000);
+
+      cm = new DefaultCacheManager(gcb.build(), b.build());
       Cache<String, String> cache = cm.getCache();
       cache.put("a", "a");
       assertEquals("a", cache.get("a"));
--- integrationtests/server-integration/third-party-server/src/test/java/org/infinispan/test/integration/thirdparty/embedded/InfinispanCoreIT.java
+++ integrationtests/server-integration/third-party-server/src/test/java/org/infinispan/test/integration/thirdparty/embedded/InfinispanCoreIT.java
@@ -1,10 +1,12 @@
 package org.infinispan.test.integration.thirdparty.embedded;
 
-import org.infinispan.test.integration.thirdparty.DeploymentHelper;
 import org.infinispan.test.integration.embedded.AbstractInfinispanCoreIT;
+import org.infinispan.test.integration.thirdparty.DeploymentHelper;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
 
@@ -21,9 +23,33 @@ public class InfinispanCoreIT extends AbstractInfinispanCoreIT {
    public static Archive<?> deployment() {
       WebArchive war = DeploymentHelper.createDeployment();
       war.addClass(AbstractInfinispanCoreIT.class);
-
+      war.add(permission(), "META-INF/permissions.xml");
       DeploymentHelper.addLibrary(war, "org.infinispan:infinispan-core");
-
       return war;
+   }
+
+   private static Asset permission() {
+      return new StringAsset("<permissions version=\"7\">\n" +
+            "  <permission>\n" +
+            "  <class-name>java.lang.reflect.ReflectPermission</class-name>\n" +
+            "  <name>suppressAccessChecks</name>\n" +
+            "  <actions>read,write</actions>\n" +
+            "  </permission>\n" +
+            "  <permission>\n" +
+            "  <class-name>java.lang.RuntimePermission</class-name>\n" +
+            "  <name>*</name>\n" +
+            "  <actions>read,write</actions>\n" +
+            "  </permission>\n" +
+            "  <permission>\n" +
+            "  <class-name>java.util.PropertyPermission</class-name>\n" +
+            "  <name>*</name>\n" +
+            "  <actions>read,write</actions>\n" +
+            "  </permission>\n" +
+            "  <permission>\n" +
+            "  <class-name>java.io.FilePermission</class-name>\n" +
+            "  <name>META-INF/infinispan-features.properties</name>\n" +
+            "  <actions>read,write</actions>\n" +
+            "  </permission>\n" +
+            "</permissions>");
    }
 }
